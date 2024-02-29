e457476bb831eb33469f37863bcc13668ba68d02
EVANDRO DE MACEDO SANTANA <evandromsantana@hotmail.com>
2024-02-29 15:46:27 -0300

    create: greeting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 const Page = () => {
+  const fullTime = new Intl.DateTimeFormat("pt-BR", {
+    timeStyle: "short",
+    hour12: false,
+  }).format();
+
+  const hour = new Date().getHours();
+  let greeting = "";
+
+  if (hour >= 0 && hour < 12) {
+    greeting = "Bom dia 😁";
+  } else if (hour >= 12 && hour < 18) {
+    greeting = "Boa Tarde 😎";
+  } else if (hour >= 18 && hour <= 23) {
+    greeting = "Boa Noite 😴";
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="text-9xl">00:00</div>
-      <div className="text-5xl font-bold">Bom dia</div>
+      <div className="text-9xl">{fullTime}</div>
+      <div className="text-5xl font-bold">{greeting}</div>
     </div>
   );
 };
