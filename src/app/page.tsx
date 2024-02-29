454cc6673bdda8cb7e6373a20700e4ca8ba27c66
EVANDRO DE MACEDO SANTANA <evandromsantana@hotmail.com>
2024-02-29 16:16:56 -0300

    refactor: function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,18 @@ const Page = () => {
   }).format();
 
   const hour = new Date().getHours();
-  let greeting = "";
-
-  if (hour >= 0 && hour < 12) {
-    greeting = "Bom dia 😁";
-  } else if (hour >= 12 && hour < 18) {
-    greeting = "Boa Tarde 😎";
-  } else if (hour >= 18 && hour <= 23) {
-    greeting = "Boa Noite 😴";
-  }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">{greeting}</div>
+    <div
+      className="w-screen h-screen flex flex-col justify-center items-center
+     text-white bg-gradient-to-r from-sky-500 to-indigo-500"
+    >
+      <div className="text-9xl font-bold bg-">{fullTime}</div>
+      <div className="text-5xl font-bold">
+        {hour >= 0 && hour < 12 && "Bom dia 😁"}
+        {hour >= 12 && hour < 18 && "Boa Tarde 😎"}
+        {hour >= 18 && hour <= 23 && "Bo Noite 😴"}
+      </div>
     </div>
   );
 };
